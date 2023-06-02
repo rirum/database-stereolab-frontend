1c5f5aece9f0e4ec2ad0734a4225c5bdb1549d1e
camille <cerulean.dsgn@gmail.com>
2023-06-02 12:49:17 -0300

    feat: starting project

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+
+
+function App() {
+
+
+  return (
+    <>
+  <ContainerApp>
+    teste
+  </ContainerApp>
+      </>
+  )
+}
+
+export default App
+
+const ContainerApp = styled.div`
+width:100vw;
+min-height: 100vh;
+background-color: #393E46;
+margin: 0px;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 0px;
+padding: 0px;
+font-family: 'Bebas-Neue', sans-serif;
+
+`
