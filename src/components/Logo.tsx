0cb1600c9e028d62f03a880d474f8339eafec266
camille <cerulean.dsgn@gmail.com>
2023-06-05 22:20:01 -0300

    feat: contact page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function Logo(){
     return(
         <>
+        <LogoLink to='/'>
         <StyledLogo>
         <h1>Stereolab</h1>
         </StyledLogo>
+        </LogoLink>
         
         </>
     )
@@ -16,4 +19,8 @@ const StyledLogo = styled.div`
     color: #C84B31;
     font-family: 'Bebas Neue', sans-serif;
     margin-top: 160px;
+   
+`
+const LogoLink = styled(Link)`
+    text-decoration: none;
 `
--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,12 +1,66 @@
 import styled from 'styled-components'
 import Logo from '../components/Logo'
 import Menu from '../components/Menu'
+import { Link } from 'react-router-dom';
+import { MdOutlineEmail } from "react-icons/md";
+import { FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa";
 
 export default function Contato() {
     return (
         <>
             <Logo />
+            
+            <WrapperSocial>
+            <WrapperIcon><StyledMail/> <SocialLink to="#"><p>E-mail</p></SocialLink></WrapperIcon>
+            <WrapperIcon><StyledInstagram/><SocialLink to="#"><p>Instagram</p></SocialLink></WrapperIcon>
+            <WrapperIcon><StyledTikTok/><SocialLink to="#"><p>TikTok</p></SocialLink></WrapperIcon>
+            <WrapperIcon><StyledYoutube/><SocialLink to="#"><p>Youtube</p></SocialLink></WrapperIcon>
+           
+            </WrapperSocial>
             <Menu />
         </>
     )
 }
+
+const WrapperSocial = styled.div`
+    width: 600px;
+    height: 150px;
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 100px;
+    margin-bottom: 200px;
+`
+const WrapperIcon = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom:15px;
+    align-items: center;
+    p{
+        font-family: 'Bebas Neue', sans-serif;
+        color: #ECDBBA;
+        font-size: 30px;
+        margin-top: 20px;
+    }
+`
+const SocialLink = styled(Link)`
+    text-decoration: none;
+`
+const StyledMail = styled(MdOutlineEmail)`
+    font-size: 4.5rem;
+    color: #C84B31;
+`
+const StyledInstagram = styled(FaInstagram)`
+    font-size: 4.5rem;
+    color: #C84B31;
+`
+const StyledTikTok = styled(FaTiktok)`
+    font-size: 4.5rem;
+    color: #C84B31;
+`
+
+const StyledYoutube = styled(FaYoutube)`
+    font-size: 4.5rem;
+    color: #C84B31;
+`
+
+
